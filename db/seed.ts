f43435dc45e0b2804a93dbd18f7e0334f27c23b4
Nick Allen <rutherford.t.b@gmail.com>
2024-04-09 14:22:32 -0500

    add local test user

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,18 @@
-// import { db } from 'astro:db';
+import { db, User } from "astro:db";
+import { generateId } from "lucia";
+import { Argon2id } from "oslo/password";
 
 // https://astro.build/db/seed
 export default async function seed() {
-    // TODO
+    // Seed local test user
+    const username = "testuser";
+    const password = "testpassword";
+    const userId = generateId(15);
+    const hashedPassword = await new Argon2id().hash(password);
+
+    await db
+        .insert(User)
+        .values([
+            { id: userId, username: username, hashed_password: hashedPassword },
+        ]);
 }
