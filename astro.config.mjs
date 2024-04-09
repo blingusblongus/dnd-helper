a2e51108b1819b60ef036026a19a8fd7cd0ea5b6
Nick Allen <rutherford.t.b@gmail.com>
2024-04-09 13:13:17 -0500

    add shadcn, react, tailwind, prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 import { defineConfig } from 'astro/config';
 import db from "@astrojs/db";
-
 import vercel from "@astrojs/vercel/serverless";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    optimizeDeps: {
-      exclude: ["astro:db", "oslo"]
-    }
-  },
-  integrations: [db()],
-  output: "server",
-  adapter: vercel()
+    vite: {
+        optimizeDeps: {
+            exclude: ["astro:db", "oslo"]
+        }
+    },
+    integrations: [db(), react(), tailwind({
+        applyBaseStyles: false,
+    })],
+    output: "server",
+    adapter: vercel()
 });
